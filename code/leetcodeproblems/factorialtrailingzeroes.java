60042105a221104ca7e27bb3594966a4cfa1ce23
Gopal Soni <gopal4522soni@gmail.com>
2025-06-27 21:57:38 +0530

    27/06/25

--- code/leetcodeproblems/factorialtrailingzeroes.java
+++ code/leetcodeproblems/factorialtrailingzeroes.java
@@ -6,6 +6,12 @@ public class factorialtrailingzeroes {
     public static int trailingZeroes(int n) {
 
       
+        int count = 0;
+        while (n >= 5) {
+            n /= 5;          // divide n by 5
+            count += n;      // add the result to count
+        }
+        return count;
 
         
     }
