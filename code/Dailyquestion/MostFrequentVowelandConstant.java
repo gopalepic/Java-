059d1065d2204230338c97961b66b3ee07106e3e
Gopal Soni <gopal4522soni@gmail.com>
2025-09-13 23:14:21 +0530

    more questions

---
+++ code/Dailyquestion/MostFrequentVowelandConstant.java
@@ -0,0 +1,58 @@
+package code.Dailyquestion;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class MostFrequentVowelandConstant {
+
+    public static void main(String[] args) {
+        String s = "successes";
+        int result =  maxFreqSum(s);
+        System.out.println("RESULt " + result);
+    }
+
+    public static int maxFreqSum(String s) {
+
+        HashMap<Character, Integer> Vowels = new HashMap<>();
+        HashMap<Character, Integer> Consonats = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+            if ( ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                if(Vowels.containsKey(ch)){
+                    Vowels.put(ch,Vowels.getOrDefault(ch,1)+1);
+                } else {
+                    Vowels.put(ch, 1);
+                }
+            }
+             else {
+               if (Consonats.containsKey(ch)) {
+                    Consonats.put(ch,Consonats.getOrDefault(ch , 1) +1);
+                } else {
+                    Consonats.put(ch, 1);
+                } 
+        }
+        }
+
+        int maxValueInConsonants = 0;
+        for(int val : Consonats.values()){
+            if(val > maxValueInConsonants){
+                maxValueInConsonants = val;
+            }
+        }
+
+        int maxValueInVowels = 0;
+        for(int val : Vowels.values()){
+            if(val> maxValueInVowels){
+                maxValueInVowels = val;
+            }
+        }
+        
+
+
+        return  maxValueInConsonants+maxValueInVowels;
+    
+}
+
+}
